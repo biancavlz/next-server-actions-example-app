1534ea55976f8275c7f77e15e116d9c2c43f5f01
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-25 15:12:03 +0200

    Add post submition with server actions and save in DB

---
+++ app/feed/page.tsx
@@ -0,0 +1,13 @@
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export default async function FeedPage() {
+  const posts = await getPosts(10);
+
+  return (
+    <>
+      <h1>All posts by all users</h1>
+      <Posts posts={posts} />
+    </>
+  );
+}
