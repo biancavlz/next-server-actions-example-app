acd6a38aa466999990e64d629deed85c62b22226
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-29 14:11:03 +0200

    Add dynamic metadata

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,6 +1,21 @@
 import Posts from "@/components/posts";
 import { getPosts } from "@/lib/posts";
 
+type Props = {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata() {
+  const posts = await getPosts(5);
+  const numPosts = posts.length;
+
+  return {
+    title: `Browse all our ${numPosts} posts`,
+    description: "Browse all our posts",
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts(10);
 
